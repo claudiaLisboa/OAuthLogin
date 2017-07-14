2ef6b2d526a8afb4009c1b4c9eebb3c2f42c902b
Claudia <ccalumby@gmail.com>
2017-07-14 10:25:42 -0400

    Initial comit

---
+++ backend/config.js
@@ -0,0 +1,12 @@
+export const facebook = {
+  clientID: '1252859358157940',
+  clientSecret: '80b2a9ae2e8a673f0486a3607d6b899d',
+  callbackURL: 'http://localhost:3000/auth/facebook/callback',
+  profileFields: ['id', 'name', 'displayName', 'picture', 'email'],
+};
+
+export const google = {
+  clientID: '1042632925713-l313t8qgughlgg4p96picd8fvcmabqsn.apps.googleusercontent.com',
+  clientSecret: 'paz0RTchy76srU4yfWQ7PG0n',
+  callbackURL: 'http://localhost:3000/auth/google/callback',
+};
